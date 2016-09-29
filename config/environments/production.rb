0b63f38e61bd0e7b370025d4ad360aa93735b9a3
Mohan kumar <mohankumar.m@ticketsimply.com>
2016-09-29 22:25:43 +0530

    production.rb changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,9 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  config.app_domain = 'localhost'
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
